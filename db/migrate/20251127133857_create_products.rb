64cbb912e135de26b28541debb5576352a6c6e18
Melih <80722009+meliherz@users.noreply.github.com>
2025-11-27 16:43:55 +0300

    feat(product): add scaffold for Product with basic CRUD and fields

---
+++ db/migrate/20251127133857_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :stock
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
