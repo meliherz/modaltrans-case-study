5582994eb734949f4c80a265d36a0d7389f8723f
Melih <80722009+meliherz@users.noreply.github.com>
2025-11-28 20:45:40 +0300

    feat(routes): define collection route for syncing products from DB to Google Sheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :products do
     post :sync, on: :collection
+    post :sync_to_sheet, on: :collection
   end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
